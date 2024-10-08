e74b876f49e2ad56999a1ea9c5e0c09e1772be9d
PseudoIceCreamSandwich <youssef.abdelsalam1234@gmail.com>
2024-10-09 02:35:37 +0400

    finished it

--- index.js
+++ index.js
@@ -6,7 +6,30 @@ const port = 3000;
 
 let csvData = [];
 
-app.get("/api/demand", (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/demand", async (req, res) => {
+  function convertStringToDate(dateString) {
+    // Split the string into day, month, and year
+    const [day, month, year] = dateString.split("/");
+
+    // Create a new Date object
+    return new Date(year, month - 1, day);
+  }
+  function convert(input) {
+    // Remove outer square brackets
+    const cleanedInput = input.replace(/^\[|\]$/g, "");
+
+    // Split into individual tuples
+    const tuples = cleanedInput.split(", ");
+
+    // Map each tuple to an array of floats
+    return tuples.map((tuple) =>
+      tuple.split(",").map((part) => parseFloat(part.trim()))
+    );
+  }
   let result;
   let events;
   const filePath = "./taxidemand2.csv";
@@ -15,24 +38,39 @@ app.get("/api/demand", (req, res) => {
     .on("data", (data) => {
       csvData.push(data);
     })
-    .on("end", () => {
+    .on("end", async () => {
       // Perform your query here
       result = csvData.filter((row) => row["StartHour"] == req.query.hour);
+      console.log(convert(result[0]["Dist"]));
+      locations = convert(result[0]["Dist"]);
+      csvData = [];
+
+      fs.createReadStream("./Volunteers_Activities_And_Events.csv")
+        .pipe(csv())
+        .on("data", (data) => {
+          csvData.push(data);
+        })
+        .on("end", () => {
+          // Perform your query here
+          const dateObj = new Date(req.query.date);
+          events = csvData.filter((event) => {
+            const startDate = new Date(event.eventstartingdate);
+            const endDate = new Date(event.eventendingdate);
+            const introDate = new Date(event.introductiondate);
+
+            return (
+              (introDate >= dateObj && startDate <= dateObj) ||
+              (startDate >= dateObj && endDate <= dateObj) ||
+              (introDate <= dateObj && endDate >= dateObj)
+            );
+          });
+
+          console.log(locations);
+          console.log(events);
+
+          res.json({ locations, events });
+        });
     });
-  fs.createReadStream("./Volunteers_Activities_And_Events.csv")
-    .pipe(csv())
-    .on("data", (data) => {
-      csvData.push(data);
-    })
-    .on("end", () => {
-      // Perform your query here
-      events = csvData.filter(
-        (row) =>
-          row["eventstartingdate"] < req.query.date &&
-          row["eventendingdate"] > req.query.date
-      );
-    });
-  res.json(result, events);
 });
 
 app.listen(port, () => {
